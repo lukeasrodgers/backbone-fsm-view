d3c3903c3e32336fd05602c65af52936e2375d73
Luke Rodgers <lukeasrodgers@gmail.com>
2012-06-04 06:17:16 -0400

    Adding first test.

--- test/fsm_view_test.js
+++ test/fsm_view_test.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
-	module('FsmView');
+	module('translateDomEventToFsm', {
+    setup: function() {
+      this.$el = jQuery('<div id="test-div"><input class="fsm-event-trigger" data-event-name="foobar" /></div>');
+      this.$el.appendTo('body');
+      this.view = new FsmView({el: this.$el});
+    },
+    teardown: function() {
+      $('#test-div').detach();
+    }
+  });
 	
-	test('translateDomEventToFsm', function() {
-		var view = new FsmView();
-		
+	test('translates a DOM change event into an FSM event', function() {
+    var stub = this.stub(this.view, 'process');
+    var e = jQuery.Event('change');
+    this.$el.find('input').trigger(e);
+    sinon.assert.calledOnce(spy);
 	});
 
 });
